152f6b7b60fd8d5090aaade8bc23bc2ed0d6ed00
John Hendrikx <john.hendrikx+github@gmail.com>
2025-12-01 16:43:20 +0100

    Add additional tests to architecture test

--- common-collection/src/test/java/org/int4/common/collection/ArchitectureTest.java
+++ common-collection/src/test/java/org/int4/common/collection/ArchitectureTest.java
@@ -24,15 +24,21 @@
 
 package org.int4.common.collection;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 import com.tngtech.archunit.library.DependencyRules;
 
 import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Modifier;
 
 import org.junit.jupiter.api.Test;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,6 +51,24 @@ public class ArchitectureTest {
 
   @ArchTest
   private final ArchRule noClassesShouldDependOnUpperPackages = DependencyRules.NO_CLASSES_SHOULD_DEPEND_UPPER_PACKAGES;
+
+  @ArchTest
+  private final ArchRule noClassesShouldPubliclyImplementInterfaceInSamePackage = noClasses().should(publiclyImplementInterfacesInSamePackage());
+
+  private static ArchCondition<JavaClass> publiclyImplementInterfacesInSamePackage() {
+    return new ArchCondition<>("publicly implement interfaces that reside in same package") {
+      @Override
+      public void check(JavaClass cls, ConditionEvents events) {
+        for(JavaClass iface : cls.getAllRawInterfaces()) {
+          boolean isSamePackageAndPublic = iface.getPackage().equals(cls.getPackage())
+            && Modifier.isPublic(cls.reflect().getModifiers())
+            && !Modifier.isAbstract(cls.reflect().getModifiers());
+
+          events.add(new SimpleConditionEvent(iface, isSamePackageAndPublic, cls.getDescription() + " is public and not abstract and implements <" + iface.getFullName() + "> in same package"));
+        }
+      }
+    };
+  }
 
   @Test
   void shouldMatchPackageName() {
